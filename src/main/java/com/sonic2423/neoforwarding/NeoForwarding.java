b4f1820a53ec2c7aba44a22e2c4f2a3070db8c85
Sonic2423 <super2423sonic@gmail.com>
2024-08-13 16:49:16 +0200

    update MDK

--- src/main/java/com/sonic2423/neoforwarding/NeoForwarding.java
+++ src/main/java/com/sonic2423/neoforwarding/NeoForwarding.java
@@ -3,7 +3,6 @@ package com.sonic2423.neoforwarding;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import org.slf4j.Logger;
