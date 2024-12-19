eea5db138926f020c1bdf9ac3638b329255b4d88
Sonic2423 <super2423sonic@gmail.com>
2024-12-19 09:47:48 +0100

    fix possible leak of secret; merge previous 1.21.X fixes; update mdk

--- src/main/java/com/sonic2423/neoforwarding/NeoForwarding.java
+++ src/main/java/com/sonic2423/neoforwarding/NeoForwarding.java
@@ -19,6 +19,6 @@ public class NeoForwarding {
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public NeoForwarding(IEventBus modEventBus) {
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC, "neoforwarding-server.toml");
     }
 }
--- src/main/java/com/sonic2423/neoforwarding/mixin/mixins/CrossStitchSupport.java
+++ src/main/java/com/sonic2423/neoforwarding/mixin/mixins/CrossStitchSupport.java
@@ -6,6 +6,7 @@ import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.registries.RegistryManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -32,7 +33,7 @@ public abstract class CrossStitchSupport {
         ResourceLocation key = BuiltInRegistries.COMMAND_ARGUMENT_TYPE.getKey(pArgumentInfo);
         int id = BuiltInRegistries.COMMAND_ARGUMENT_TYPE.getId(pArgumentInfo);
 
-        if (key == null || key.getNamespace().equals("brigadier") || (key.getNamespace().equals("minecraft") && !key.getPath().equals("test_argument") && !key.getPath().equals("test_class"))) {
+        if (key == null || neoforwarding$isVanillaRegistry(key)) {
             return;
         }
 
@@ -54,6 +55,12 @@ public abstract class CrossStitchSupport {
 
         pBuffer.writeVarInt(extraData.readableBytes());
         pBuffer.writeBytes(extraData);
+    }
+
+    @Unique
+    private static boolean neoforwarding$isVanillaRegistry(ResourceLocation location) {
+        // Checks if the registry name is contained within the static view of VanillaRegistries
+        return RegistryManager.getVanillaRegistryKeys().contains(location);
     }
 }
 
